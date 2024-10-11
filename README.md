7e8ac4240f5339e89d1fa31eadcfda3d01d98d36
Thomas <thomas.bnfls@gmail.com>
2024-10-11 15:01:04 +0200

    init project

--- README.md
+++
@@ -1,2 +0,0 @@
-# turbo-conquest
-# turbo-conquest
---
+++ apps/app/src/components/sign-out.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { createClient } from "@conquest/supabase/client";
+import { Button } from "@conquest/ui/button";
+import { Icons } from "@conquest/ui/icons";
+
+export function SignOut() {
+  const supabase = createClient();
+
+  const handleSignOut = () => {
+    supabase.auth.signOut();
+  };
+
+  return (
+    <Button
+      onClick={handleSignOut}
+      variant="outline"
+      className="font-mono gap-2 flex items-center"
+    >
+      <Icons.SignOut className="size-4" />
+      <span>Sign out</span>
+    </Button>
+  );
+}
