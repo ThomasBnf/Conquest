5d5c37c6440c04db68395d91d16033189550707a
Thomas <thomas.bnfls@gmail.com>
2024-10-17 12:07:38 +0200

    change slack port

--- apps/bots/slack.ts
+++ apps/bots/slack.ts
@@ -18,7 +18,7 @@ import { deleteReactions } from "./queries/reactions/deleteReactions";
 dotenv.config();
 
 const appExpress = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3100;
 
 const token = process.env.SLACK_BOT_TOKEN;
 const signingSecret = process.env.SLACK_SIGNING_SECRET;
