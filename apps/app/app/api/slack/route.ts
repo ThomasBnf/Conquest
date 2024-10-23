238c8cbb8f7af75f46f65634becaded90147c4f5
Thomas <thomas.bnfls@gmail.com>
2024-10-23 14:53:15 +0200

    challenge slack

--- apps/app/app/api/slack/route.ts
+++ apps/app/app/api/slack/route.ts
@@ -1,257 +1,248 @@
-import { createActivity } from "@/actions/bot/activities/createActivity";
-import { deleteActivity } from "@/actions/bot/activities/deleteActivity";
-import { updateActivity } from "@/actions/bot/activities/updateActivity";
-import { createChannel } from "@/actions/bot/channels/createChannel";
-import { deleteChannel } from "@/actions/bot/channels/deleteChannel";
-import { getChannel } from "@/actions/bot/channels/getChannel";
-import { updateChannel } from "@/actions/bot/channels/updateChannel";
-import { getIntegration } from "@/actions/bot/integrations/getIntegration";
-import { updateIntegration } from "@/actions/bot/integrations/updateIntegration";
-import { mergeMember } from "@/actions/bot/members/mergeMember";
-import { updateMember } from "@/actions/bot/members/updateMember";
-import { deleteReactions } from "@/actions/bot/reactions/deleteReactions";
 import { safeRoute } from "@/lib/safeRoute";
-import {
-  type GenericMessageEvent,
-  type SlackEvent,
-  WebClient,
-} from "@slack/web-api";
+import { WebClient } from "@slack/web-api";
 import { NextResponse } from "next/server";
 import { z } from "zod";
 
 const web = new WebClient(process.env.SLACK_BOT_TOKEN);
 
 const bodySchema = z.object({
-  team_id: z.string(),
-  event: z.custom<SlackEvent>(),
+  // team_id: z.string(),
+  // event: z.custom<SlackEvent>(),
+  challenge: z.string().optional(),
 });
 
 export const POST = safeRoute.body(bodySchema).handler(async (_, context) => {
-  const { team_id, event } = context.body;
-  const { type } = event;
+  const { challenge } = context.body;
 
-  console.log(event);
-
-  const rIntegration = await getIntegration({ external_id: team_id });
-  const integration = rIntegration?.data;
-  const workspace_id = integration?.workspace_id;
-
-  if (!workspace_id) {
-    return NextResponse.json(
-      { message: "Integration not found" },
-      { status: 404 },
-    );
+  if (challenge) {
+    return NextResponse.json({ challenge });
   }
 
-  switch (type) {
-    case "app_uninstalled": {
-      await updateIntegration({
-        external_id: team_id,
-        status: "DISCONNECTED",
-      });
-      break;
-    }
+  // const { team_id, event } = context.body;
+  // const { type } = event;
 
-    case "channel_created": {
-      const { name, id } = event.channel;
-      const rChannel = await createChannel({
-        name,
-        external_id: id,
-        source: "SLACK",
-        workspace_id,
-      });
-      if (!rChannel?.data) {
-        return NextResponse.json(
-          { message: "Channel not created" },
-          { status: 400 },
-        );
-      }
-      await web.conversations.join({ channel: id });
-      break;
-    }
+  // console.log(event);
 
-    case "channel_rename": {
-      const { name, id } = event.channel;
-      await updateChannel({ external_id: id, name });
-      break;
-    }
+  // const rIntegration = await getIntegration({ external_id: team_id });
+  // const integration = rIntegration?.data;
+  // const workspace_id = integration?.workspace_id;
 
-    case "channel_deleted":
-      await deleteChannel({ external_id: event.channel });
-      break;
+  // if (!workspace_id) {
+  //   return NextResponse.json(
+  //     { message: "Integration not found" },
+  //     { status: 404 },
+  //   );
+  // }
 
-    case "member_joined_channel":
-      await mergeMember({ web, user: event.user, workspace_id });
-      break;
+  // switch (type) {
+  //   case "app_uninstalled": {
+  //     await updateIntegration({
+  //       external_id: team_id,
+  //       status: "DISCONNECTED",
+  //     });
+  //     break;
+  //   }
 
-    case "message": {
-      const { channel: channel_id, subtype } = event;
+  //   case "channel_created": {
+  //     const { name, id } = event.channel;
+  //     const rChannel = await createChannel({
+  //       name,
+  //       external_id: id,
+  //       source: "SLACK",
+  //       workspace_id,
+  //     });
+  //     if (!rChannel?.data) {
+  //       return NextResponse.json(
+  //         { message: "Channel not created" },
+  //         { status: 400 },
+  //       );
+  //     }
+  //     await web.conversations.join({ channel: id });
+  //     break;
+  //   }
 
-      const handleMessage = async (messageEvent: GenericMessageEvent) => {
-        const { user, text, thread_ts, ts, files } = messageEvent;
+  //   case "channel_rename": {
+  //     const { name, id } = event.channel;
+  //     await updateChannel({ external_id: id, name });
+  //     break;
+  //   }
 
-        if (!user) {
-          return NextResponse.json(
-            { message: "User not found" },
-            { status: 404 },
-          );
-        }
+  //   case "channel_deleted":
+  //     await deleteChannel({ external_id: event.channel });
+  //     break;
 
-        const rMember = await mergeMember({ web, user, workspace_id });
-        const member = rMember?.data;
+  //   case "member_joined_channel":
+  //     await mergeMember({ web, user: event.user, workspace_id });
+  //     break;
 
-        if (!member) {
-          return NextResponse.json(
-            { message: "Member not found" },
-            { status: 404 },
-          );
-        }
+  //   case "message": {
+  //     const { channel: channel_id, subtype } = event;
 
-        const rChannel = await getChannel({
-          external_id: channel_id,
-          workspace_id,
-        });
-        const channel = rChannel?.data;
+  //     const handleMessage = async (messageEvent: GenericMessageEvent) => {
+  //       const { user, text, thread_ts, ts, files } = messageEvent;
 
-        if (!channel) {
-          return NextResponse.json(
-            { message: "Channel not found" },
-            { status: 404 },
-          );
-        }
+  //       if (!user) {
+  //         return NextResponse.json(
+  //           { message: "User not found" },
+  //           { status: 404 },
+  //         );
+  //       }
 
-        await createActivity({
-          member_id: member.id,
-          channel_id: channel.id,
-          details: {
-            message: text ?? "",
-            source: "SLACK",
-            type: thread_ts ? "REPLY" : "MESSAGE",
-            files:
-              files?.map(({ title, url_private }) => ({
-                title: title ?? "",
-                url: url_private ?? "",
-              })) ?? [],
-            ts,
-          },
-          workspace_id,
-        });
-      };
+  //       const rMember = await mergeMember({ web, user, workspace_id });
+  //       const member = rMember?.data;
 
-      switch (subtype) {
-        case undefined:
-        case "file_share": {
-          await handleMessage(event as GenericMessageEvent);
-          break;
-        }
+  //       if (!member) {
+  //         return NextResponse.json(
+  //           { message: "Member not found" },
+  //           { status: 404 },
+  //         );
+  //       }
 
-        case "message_changed": {
-          const { text, ts, thread_ts, files } =
-            event.message as GenericMessageEvent;
-          await updateActivity({
-            ts,
-            details: {
-              message: text ?? "",
-              source: "SLACK",
-              type: thread_ts ? "REPLY" : "MESSAGE",
-              files:
-                files?.map(({ title, url_private }) => ({
-                  title: title ?? "",
-                  url: url_private ?? "",
-                })) ?? [],
-              ts,
-            },
-          });
-          break;
-        }
+  //       const rChannel = await getChannel({
+  //         external_id: channel_id,
+  //         workspace_id,
+  //       });
+  //       const channel = rChannel?.data;
 
-        case "message_deleted": {
-          const { deleted_ts } = event;
-          const rChannel = await getChannel({
-            external_id: channel_id,
-            workspace_id,
-          });
-          const channel = rChannel?.data;
+  //       if (!channel) {
+  //         return NextResponse.json(
+  //           { message: "Channel not found" },
+  //           { status: 404 },
+  //         );
+  //       }
 
-          if (!channel) {
-            return NextResponse.json(
-              { message: "Channel not found" },
-              { status: 404 },
-            );
-          }
+  //       await createActivity({
+  //         member_id: member.id,
+  //         channel_id: channel.id,
+  //         details: {
+  //           message: text ?? "",
+  //           source: "SLACK",
+  //           type: thread_ts ? "REPLY" : "MESSAGE",
+  //           files:
+  //             files?.map(({ title, url_private }) => ({
+  //               title: title ?? "",
+  //               url: url_private ?? "",
+  //             })) ?? [],
+  //           ts,
+  //         },
+  //         workspace_id,
+  //       });
+  //     };
 
-          await deleteReactions({ channel_id: channel.id, ts: deleted_ts });
-          await deleteActivity({ channel_id: channel.id, ts: deleted_ts });
-          break;
-        }
-      }
-      break;
-    }
+  //     switch (subtype) {
+  //       case undefined:
+  //       case "file_share": {
+  //         await handleMessage(event as GenericMessageEvent);
+  //         break;
+  //       }
 
-    case "reaction_added": {
-      const { user, item, reaction } = event;
-      const { channel: channel_id, ts } = item;
+  //       case "message_changed": {
+  //         const { text, ts, thread_ts, files } =
+  //           event.message as GenericMessageEvent;
+  //         await updateActivity({
+  //           ts,
+  //           details: {
+  //             message: text ?? "",
+  //             source: "SLACK",
+  //             type: thread_ts ? "REPLY" : "MESSAGE",
+  //             files:
+  //               files?.map(({ title, url_private }) => ({
+  //                 title: title ?? "",
+  //                 url: url_private ?? "",
+  //               })) ?? [],
+  //             ts,
+  //           },
+  //         });
+  //         break;
+  //       }
 
-      const rMember = await mergeMember({ web, user, workspace_id });
-      const member = rMember?.data;
+  //       case "message_deleted": {
+  //         const { deleted_ts } = event;
+  //         const rChannel = await getChannel({
+  //           external_id: channel_id,
+  //           workspace_id,
+  //         });
+  //         const channel = rChannel?.data;
 
-      if (!member) {
-        return NextResponse.json(
-          { message: "Member not found" },
-          { status: 404 },
-        );
-      }
+  //         if (!channel) {
+  //           return NextResponse.json(
+  //             { message: "Channel not found" },
+  //             { status: 404 },
+  //           );
+  //         }
 
-      const rChannel = await getChannel({
-        external_id: channel_id,
-        workspace_id,
-      });
-      const channel = rChannel?.data;
+  //         await deleteReactions({ channel_id: channel.id, ts: deleted_ts });
+  //         await deleteActivity({ channel_id: channel.id, ts: deleted_ts });
+  //         break;
+  //       }
+  //     }
+  //     break;
+  //   }
 
-      if (!channel) {
-        return NextResponse.json(
-          { message: "Channel not found" },
-          { status: 404 },
-        );
-      }
+  //   case "reaction_added": {
+  //     const { user, item, reaction } = event;
+  //     const { channel: channel_id, ts } = item;
 
-      await createActivity({
-        member_id: member.id,
-        channel_id: channel.id,
-        details: {
-          source: "SLACK",
-          type: "REACTION",
-          message: reaction,
-          files: [],
-          ts,
-        },
-        workspace_id,
-      });
-      break;
-    }
+  //     const rMember = await mergeMember({ web, user, workspace_id });
+  //     const member = rMember?.data;
 
-    case "reaction_removed": {
-      const { reaction, item } = event;
-      const { ts, channel } = item;
-      await deleteActivity({ channel_id: channel, message: reaction, ts });
-      break;
-    }
+  //     if (!member) {
+  //       return NextResponse.json(
+  //         { message: "Member not found" },
+  //         { status: 404 },
+  //       );
+  //     }
 
-    case "user_change": {
-      const { profile, id } = event.user;
-      const { first_name, last_name, title, phone, image_1024 } = profile;
+  //     const rChannel = await getChannel({
+  //       external_id: channel_id,
+  //       workspace_id,
+  //     });
+  //     const channel = rChannel?.data;
 
-      await updateMember({
-        slack_id: id,
-        first_name,
-        last_name,
-        job_title: title,
-        phone,
-        avatar_url: image_1024,
-      });
-      break;
-    }
-  }
+  //     if (!channel) {
+  //       return NextResponse.json(
+  //         { message: "Channel not found" },
+  //         { status: 404 },
+  //       );
+  //     }
+
+  //     await createActivity({
+  //       member_id: member.id,
+  //       channel_id: channel.id,
+  //       details: {
+  //         source: "SLACK",
+  //         type: "REACTION",
+  //         message: reaction,
+  //         files: [],
+  //         ts,
+  //       },
+  //       workspace_id,
+  //     });
+  //     break;
+  //   }
+
+  //   case "reaction_removed": {
+  //     const { reaction, item } = event;
+  //     const { ts, channel } = item;
+  //     await deleteActivity({ channel_id: channel, message: reaction, ts });
+  //     break;
+  //   }
+
+  //   case "user_change": {
+  //     const { profile, id } = event.user;
+  //     const { first_name, last_name, title, phone, image_1024 } = profile;
+
+  //     await updateMember({
+  //       slack_id: id,
+  //       first_name,
+  //       last_name,
+  //       job_title: title,
+  //       phone,
+  //       avatar_url: image_1024,
+  //     });
+  //     break;
+  //   }
+  // }
 
   return NextResponse.json({ message: "OK" });
 });
