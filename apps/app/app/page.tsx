93190faeb0656ce8d7a9cc9b20e5c01719459df7
Thomas <thomas.bnfls@gmail.com>
2024-10-25 11:59:42 +0200

    delete page from root

--- apps/app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getCurrentUser } from "@/helpers/getCurrentUser";
-import { redirect } from "next/navigation";
-
-export default async function Page() {
-  const user = await getCurrentUser();
-
-  if (!user.onboarding) redirect("/");
-  redirect(`/${user?.workspace.slug}`);
-}
