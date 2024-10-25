2a8ea04d7f050477382dee93cc9677f5845398ef
Thomas <thomas.bnfls@gmail.com>
2024-10-25 10:38:58 +0200

    change fetching oauth

--- apps/app/features/slack/actions/oauthV2.ts
+++ apps/app/features/slack/actions/oauthV2.ts
@@ -2,7 +2,6 @@
 
 import { createIntegration } from "@/features/integrations/queries/createIntegration";
 import { IntegrationSchema } from "@conquest/zod/integration.schema";
-import ky from "ky";
 import { authAction } from "lib/authAction";
 import { revalidatePath } from "next/cache";
 import { z } from "zod";
@@ -20,22 +19,20 @@ export const oauthV2 = authAction
   .action(async ({ ctx, parsedInput: { code, scopes } }) => {
     const slug = ctx.user.workspace.slug;
 
-    const data = await ky
-      .post("https://slack.com/api/oauth.v2.access", {
-        body: new URLSearchParams({
-          code,
-          client_id: process.env.NEXT_PUBLIC_SLACK_CLIENT_ID!,
-          client_secret: process.env.SLACK_CLIENT_SECRET!,
-        }),
-      })
-      .json();
+    const response = await fetch("https://slack.com/api/oauth.v2.access", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        code,
+        client_id: process.env.NEXT_PUBLIC_SLACK_CLIENT_ID!,
+        client_secret: process.env.SLACK_CLIENT_SECRET!,
+      }),
+    });
 
-    console.log(data);
-
-    const { access_token, team } = data as {
-      access_token: string;
-      team: { id: string; name: string };
-    };
+    const data = await response.json();
+    const { access_token, team } = data;
 
     const rIntegration = await createIntegration({
       external_id: team.id,
