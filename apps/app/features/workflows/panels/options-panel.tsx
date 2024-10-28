ccd02f1b8fde2bfa3cc0c58fb98973f8ae726ad1
Thomas <thomas.bnfls@gmail.com>
2024-10-28 17:03:02 +0100

    add scroll to workflow sidebar

--- apps/app/features/workflows/panels/options-panel.tsx
+++ apps/app/features/workflows/panels/options-panel.tsx
@@ -1,5 +1,6 @@
 import { Badge } from "@conquest/ui/badge";
 import { Button } from "@conquest/ui/button";
+import { ScrollArea } from "@conquest/ui/scroll-area";
 import { NodeDataSchema } from "@conquest/zod/node.schema";
 import { DeleteDialog } from "components/custom/delete-dialog";
 import { Icon } from "components/icons/Icon";
@@ -17,8 +18,8 @@ export const OptionsPanel = () => {
   const { type } = data;
 
   return (
-    <div className="flex flex-1 flex-col divide-y">
-      <div className="flex h-12 items-center px-4">
+    <div className="flex flex-col divide-y h-full">
+      <div className="flex h-12 items-center px-4 shrink-0">
         <Button
           variant="ghost"
           onClick={() => {
@@ -30,26 +31,30 @@ export const OptionsPanel = () => {
           Back
         </Button>
       </div>
-      <div className="flex flex-col gap-4 p-6">
-        <div className="mb-2 flex items-center gap-2">
-          <Icon
-            name={data.icon as keyof typeof icons}
-            size={24}
-            className="text-muted-foreground"
-          />
-          <p className="font-medium">{data.label}</p>
-          <div className="space-y1">
-            <Badge variant="secondary">{data.category}</Badge>
+      <ScrollArea className="flex-grow h-[calc(100vh-10rem)]">
+        <div className="flex flex-col gap-4 p-6">
+          <div className="mb-2 flex items-center gap-2">
+            <Icon
+              name={data.icon as keyof typeof icons}
+              size={24}
+              className="text-muted-foreground"
+            />
+            <p className="font-medium">{data.label}</p>
+            <div className="space-y1">
+              <Badge variant="secondary">{data.category}</Badge>
+            </div>
           </div>
+          <Description />
+          {type === "trigger-recurring-schedule" && (
+            <RecurringScheduleOptions />
+          )}
+          {type === "list-records" && <ListRecordsOptions />}
+          {type === "webhook" && <WebhookOptions />}
+          {type === "add-tag" && <TagMemberOptions />}
+          {type === "remove-tag" && <TagMemberOptions />}
         </div>
-        <Description />
-        {type === "trigger-recurring-schedule" && <RecurringScheduleOptions />}
-        {type === "list-records" && <ListRecordsOptions />}
-        {type === "webhook" && <WebhookOptions />}
-        {type === "add-tag" && <TagMemberOptions />}
-        {type === "remove-tag" && <TagMemberOptions />}
-      </div>
-      <div className="mt-auto flex justify-end gap-2 p-4">
+      </ScrollArea>
+      <div className="mt-auto flex justify-end gap-2 p-4 shrink-0">
         <DeleteDialog
           title="Delete node"
           description="Are you sure you want to delete this node?"
