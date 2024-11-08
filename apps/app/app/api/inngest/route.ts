7d1ba57560f81b4bded222d54525dec238562c4b
Thomas <thomas.bnfls@gmail.com>
2024-11-08 22:47:02 +0100

    add inngest functions

--- apps/app/app/api/inngest/route.ts
+++ apps/app/app/api/inngest/route.ts
@@ -1,10 +1,20 @@
 import { InngestCronWorkflow } from "@/inngest/InngestCronWorkflow";
 import { InngestRunWorkflow } from "@/inngest/InngestRunWorkflow";
 import { inngest } from "@/inngest/client";
+import { InngestCreateListChannels } from "@/inngest/slack/InngestCreateListChannels";
+import { InngestCreateListMembers } from "@/inngest/slack/InngestCreateListMembers";
+import { InngestCreateMessage } from "@/inngest/slack/InngestCreateMessage";
 import { InngestInstalSlack } from "@/inngest/slack/InngestInstalSlack";
 import { serve } from "inngest/next";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [InngestRunWorkflow, InngestInstalSlack, InngestCronWorkflow],
+  functions: [
+    InngestRunWorkflow,
+    InngestCronWorkflow,
+    InngestInstalSlack,
+    InngestCreateListMembers,
+    InngestCreateListChannels,
+    InngestCreateMessage,
+  ],
 });
