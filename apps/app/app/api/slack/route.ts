e04bee30d355d9ebf7c17bd091a45d4fa2dab5a1
Thomas <thomas.bnfls@gmail.com>
2024-11-08 21:56:42 +0100

    slack route configured

--- apps/app/app/api/slack/route.ts
+++ apps/app/app/api/slack/route.ts
@@ -32,7 +32,7 @@ const bodySchema = z
   })
   .passthrough();
 
-export const POST = safeRoute.handler(async (_, context) => {
+export const POST = safeRoute.body(bodySchema).handler(async (_, context) => {
   const body = context.body;
 
   if (body.type === "url_verification") {
