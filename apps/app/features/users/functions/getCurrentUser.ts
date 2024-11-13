409ca62b103698ea437de4fd086e16bdb6e86f38
Thomas <thomas.bnfls@gmail.com>
2024-11-13 17:56:52 +0100

    update database

--- apps/app/features/users/functions/getCurrentUser.ts
+++ apps/app/features/users/functions/getCurrentUser.ts
@@ -2,6 +2,7 @@ import { auth } from "@/auth";
 import { prisma } from "@/lib/prisma";
 import { CustomError } from "@/lib/safeRoute";
 import { UserWithWorkspaceSchema } from "@conquest/zod/user.schema";
+import { redirect } from "next/navigation";
 
 export const getCurrentUser = async () => {
   const session = await auth();
@@ -25,9 +26,7 @@ export const getCurrentUser = async () => {
 
   const parsedUser = UserWithWorkspaceSchema.safeParse(user);
 
-  console.log(parsedUser.error);
-
-  // if (!parsedUser.success) redirect("/auth/login");
+  if (!parsedUser.success) redirect("/auth/login");
 
   return parsedUser.data;
 };
