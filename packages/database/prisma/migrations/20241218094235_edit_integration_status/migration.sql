8ec0455ff914157d813b9d921bab01338812fed4
ThomasBnf <thomas.bnfls@gmail.com>
2024-12-18 10:42:42 +0100

    db: new migration

---
+++ packages/database/prisma/migrations/20241218094235_edit_integration_status/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The values [INSTALLED] on the enum `STATUS` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "STATUS_new" AS ENUM ('ENABLED', 'CONNECTED', 'SYNCING', 'DISCONNECTED');
+ALTER TABLE "integrations" ALTER COLUMN "status" TYPE "STATUS_new" USING ("status"::text::"STATUS_new");
+ALTER TYPE "STATUS" RENAME TO "STATUS_old";
+ALTER TYPE "STATUS_new" RENAME TO "STATUS";
+DROP TYPE "STATUS_old";
+COMMIT;
