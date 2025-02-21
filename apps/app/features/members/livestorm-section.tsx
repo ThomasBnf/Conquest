7225c3cf693278b4d54cb2a47c8240a8103e70b0
ThomasBnf <thomas.bnfls@gmail.com>
2025-02-21 19:03:02 +0000

    add livestorm section

---
+++ apps/app/features/members/livestorm-section.tsx
@@ -0,0 +1,32 @@
+import { EditableLink } from "@/components/editable/editable-link";
+import { FieldCard } from "@/components/editable/field-card";
+import { Livestorm } from "@/components/icons/Livestorm";
+import {
+  LivestormProfileSchema,
+  type Profile,
+} from "@conquest/zod/schemas/profile.schema";
+
+type Props = {
+  profiles: Profile[] | undefined;
+};
+
+export const LivestormSection = ({ profiles }: Props) => {
+  const profile = profiles?.find(
+    (profile) => profile.attributes.source === "LIVESTORM",
+  );
+
+  if (!profile) return null;
+
+  const livestormProfile = LivestormProfileSchema.parse(profile);
+  const { external_id } = livestormProfile;
+
+  return (
+    <FieldCard icon={<Livestorm size={14} />} label="Livestorm">
+      <EditableLink
+        placeholder="No livestorm profile"
+        defaultValue={external_id}
+        editable={false}
+      />
+    </FieldCard>
+  );
+};
