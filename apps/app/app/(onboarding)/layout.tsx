ab9e4ee2b553282e8ec63c277c2b08a1a65af438
ThomasBnf <thomas.bnfls@gmail.com>
2025-03-10 20:18:04 +0100

    update bun

--- apps/app/app/(onboarding)/layout.tsx
+++ apps/app/app/(onboarding)/layout.tsx
@@ -10,6 +10,7 @@ export default async function Layout({ children }: PropsWithChildren) {
   if (!session) redirect("/auth/login");
 
   const { user } = session;
+
   const workspace = await getWorkspace({ id: user.workspace_id });
   if (user?.onboarding) redirect(`/${workspace.slug}`);
 
