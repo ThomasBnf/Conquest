7af3f725fd07bc008b066c0dd41cdede007bad1a
ThomasBnf <thomas.bnfls@gmail.com>
2025-03-10 21:31:32 +0100

    Fix

--- apps/app/app/[slug]/layout.tsx
+++ apps/app/app/[slug]/layout.tsx
@@ -27,6 +27,7 @@ export default async function Layout({
   if (user && !user.onboarding) redirect("/");
 
   const workspace = await getWorkspace({ id: user.workspace_id });
+
   if (slug !== workspace.slug) redirect(`/${workspace.slug}`);
 
   const cookieStore = await cookies();
