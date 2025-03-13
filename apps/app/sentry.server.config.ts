fe79380eb4c7aca05ff8a4f07f6c44f885ef17ab
ThomasBnf <thomas.bnfls@gmail.com>
2025-03-13 10:48:18 +0100

    delete client and edge sentry

--- apps/app/sentry.server.config.ts
+++ apps/app/sentry.server.config.ts
@@ -1,6 +1,7 @@
+import { env } from "@conquest/env";
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://e941ba1932350b755dedc043b89bfe2b@o4508954652311552.ingest.de.sentry.io/4508954653556816",
   enabled: process.env.NODE_ENV === "production",
+  dsn: env.SENTRY_DSN,
 });
