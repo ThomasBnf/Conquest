5ffea09954ec764ffdc9dfd97ea21b590cae4be5
ThomasBnf <thomas.bnfls@gmail.com>
2025-03-17 15:53:59 +0100

    edit schema stripe

--- packages/db/prisma/migrations/20250317145347_add_stripe/migration.sql
+++ packages/db/prisma/migrations/20250317145347_add_stripe/migration.sql
@@ -2,13 +2,13 @@
   Warnings:
 
   - The values [BASIC,PREMIUM,BUSINESS,ENTERPRISE] on the enum `PLAN` will be removed. If these variants are still used in the database, this will fail.
+  - You are about to drop the column `plan` on the `workspace` table. All the data in the column will be lost.
 
 */
 -- AlterEnum
 BEGIN;
 CREATE TYPE "PLAN_new" AS ENUM ('EXPLORER', 'ACTIVE', 'CONTRIBUTOR', 'AMBASSADOR');
 ALTER TABLE "workspace" ALTER COLUMN "plan" DROP DEFAULT;
-ALTER TABLE "workspace" ALTER COLUMN "plan" TYPE "PLAN_new" USING ("plan"::text::"PLAN_new");
 ALTER TYPE "PLAN" RENAME TO "PLAN_old";
 ALTER TYPE "PLAN_new" RENAME TO "PLAN";
 DROP TYPE "PLAN_old";
@@ -18,8 +18,7 @@ COMMIT;
 ALTER TABLE "user" ADD COLUMN     "avatar_url" TEXT;
 
 -- AlterTable
-ALTER TABLE "workspace" ADD COLUMN     "is_past_due" BOOLEAN NOT NULL DEFAULT false,
+ALTER TABLE "workspace" DROP COLUMN "plan",
+ADD COLUMN     "is_past_due" BOOLEAN NOT NULL DEFAULT false,
 ADD COLUMN     "stripe_customer_id" TEXT,
-ADD COLUMN     "trial_end" TIMESTAMP(3),
-ALTER COLUMN "plan" DROP NOT NULL,
-ALTER COLUMN "plan" DROP DEFAULT;
+ADD COLUMN     "trial_end" TIMESTAMP(3);
