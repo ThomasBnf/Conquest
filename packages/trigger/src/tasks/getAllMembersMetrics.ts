8709af457cb6e3fdafda40699e2e231f5ef50a64
ThomasBnf <thomas.bnfls@gmail.com>
2025-03-18 13:59:03 +0100

    fix getAllMembersMetrics

--- packages/trigger/src/tasks/getAllMembersMetrics.ts
+++ packages/trigger/src/tasks/getAllMembersMetrics.ts
@@ -1,7 +1,7 @@
 import { client } from "@conquest/clickhouse/client";
 import { listLevels } from "@conquest/clickhouse/levels/listLevels";
 import { listMembers } from "@conquest/clickhouse/members/listMembers";
-import { schemaTask } from "@trigger.dev/sdk/v3";
+import { type Context, runs, schemaTask } from "@trigger.dev/sdk/v3";
 import { z } from "zod";
 import { getMemberMetrics } from "./getMemberMetrics";
 
@@ -10,7 +10,9 @@ export const getAllMembersMetrics = schemaTask({
   schema: z.object({
     workspace_id: z.string(),
   }),
-  run: async ({ workspace_id }) => {
+  run: async ({ workspace_id }, { ctx }) => {
+    await checkPreviousRuns(ctx, workspace_id);
+
     const levels = await listLevels({ workspace_id });
     const members = await listMembers({ workspace_id });
 
@@ -20,21 +22,48 @@ export const getAllMembersMetrics = schemaTask({
       query: `ALTER TABLE log DELETE WHERE workspace_id = '${workspace_id}'`,
     });
 
-    await getMemberMetrics.batchTrigger(
-      members.map((member) => ({
-        payload: {
-          member,
-          levels,
-        },
-        options: {
-          idempotencyKey: `get-metrics-${member.id}`,
-          concurrencyKey: `metrics-${workspace_id}`,
-          queue: {
-            name: "members-processing-queue",
-            concurrencyLimit: 5,
+    const BATCH_SIZE = 500;
+
+    for (let i = 0; i < members.length; i += BATCH_SIZE) {
+      const membersBatch = members.slice(i, i + BATCH_SIZE);
+
+      await getMemberMetrics.batchTrigger(
+        membersBatch.map((member) => ({
+          payload: {
+            member,
+            levels,
           },
-        },
-      })),
-    );
+          options: {
+            idempotencyKey: `get-metrics-${member.id}`,
+            concurrencyKey: `metrics-${workspace_id}`,
+            queue: {
+              name: "members-processing-queue",
+              concurrencyLimit: 5,
+            },
+          },
+        })),
+      );
+    }
   },
 });
+
+const checkPreviousRuns = async (ctx: Context, workspace_id: string) => {
+  const allRuns = await runs.list({
+    status: "EXECUTING",
+    taskIdentifier: "get-all-members-metrics",
+  });
+
+  const currentRunId = ctx.run.id;
+  const previousTasks = allRuns.data.filter(
+    (run) =>
+      run.id !== currentRunId && run.metadata?.workspace_id === workspace_id,
+  );
+
+  if (previousTasks.length > 0) {
+    await Promise.all(
+      previousTasks.map(async (run) => {
+        await runs.cancel(run.id);
+      }),
+    );
+  }
+};
