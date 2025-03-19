6733ab9cee2d809b8e53dd9e70eff6066bec6199
ThomasBnf <thomas.bnfls@gmail.com>
2025-03-19 15:14:14 +0100

    add new Date()

--- apps/app/app/api/date/route.ts
+++ apps/app/app/api/date/route.ts
@@ -1,5 +1,6 @@
 export async function GET() {
   return Response.json({
+    date: new Date(),
     serverTimeUTC: new Date().toISOString(),
     localTime: new Date().toLocaleString(),
     timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
