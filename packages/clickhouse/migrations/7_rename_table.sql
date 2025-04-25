4193d761e76353c250ce8ed1fe9df7ecdf5b1cf8
ThomasBnf <thomas.bnfls@gmail.com>
2025-04-25 17:07:05 +0200

    fix name tables

---
+++ packages/clickhouse/migrations/7_rename_table.sql
@@ -0,0 +1,7 @@
+RENAME TABLE Activity TO activity;
+RENAME TABLE ActivityType TO activity_type;
+RENAME TABLE Channel TO channel;
+RENAME TABLE Company TO company;
+RENAME TABLE Level TO level;
+RENAME TABLE Log TO log;
+RENAME TABLE Member TO member;
