3c494cac07196bdf01cfe7dec5c9e0828da02068
ThomasBnf <thomas.bnfls@gmail.com>
2025-04-25 17:52:10 +0200

    fix db

--- packages/db/prisma/migrations/20250425155148_add_enums/migration.sql
+++ packages/db/prisma/migrations/20250425155148_add_enums/migration.sql
@@ -6,6 +6,7 @@
   - The `status` column on the `Integration` table would be dropped and recreated. This will lead to data loss if there is data in the column.
   - The `role` column on the `User` table would be dropped and recreated. This will lead to data loss if there is data in the column.
   - The `plan` column on the `Workspace` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
   - Changed the type of `source` on the `Event` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
 
 */
@@ -33,3 +34,6 @@ ADD COLUMN     "plan" "PLAN" DEFAULT 'ACTIVE';
 
 -- CreateIndex
 CREATE INDEX "Event_source_workspaceId_idx" ON "Event"("source", "workspaceId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
