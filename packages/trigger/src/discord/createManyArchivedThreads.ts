7cf1a2051ba25c2a3d983f7293fdbdbd8bfcd9f1
ThomasBnf <thomas.bnfls@gmail.com>
2025-04-30 01:46:57 +0200

    fix archivedthreads

--- packages/trigger/src/discord/createManyArchivedThreads.ts
+++ packages/trigger/src/discord/createManyArchivedThreads.ts
@@ -29,6 +29,7 @@ export const createManyArchivedThreads = async ({
   if (!externalId) return;
 
   let before: string | undefined = undefined;
+  const allThreads: APIThreadChannel[] = [];
 
   while (true) {
     const params = new URLSearchParams({
@@ -50,93 +51,7 @@ export const createManyArchivedThreads = async ({
 
       if (threads.length === 0) break;
 
-      for (const thread of threads) {
-        const { name, owner_id, thread_metadata } = thread;
-        const { create_timestamp } = thread_metadata ?? {};
-
-        if (!owner_id) continue;
-
-        let beforeMessageId: string | undefined = undefined;
-        let firstMessage: APIMessage | undefined;
-
-        while (true) {
-          const params = new URLSearchParams({
-            limit: "100",
-            ...(beforeMessageId ? { before: beforeMessageId } : {}),
-          });
-
-          const messages = (await discordClient.get(
-            `${Routes.channelMessages(thread.id)}?${params.toString()}`,
-          )) as APIMessage[];
-
-          logger.info("Archived thread messages", { messages });
-
-          if (messages.length < 100) firstMessage = messages.at(-1);
-
-          for (const message of messages) {
-            const {
-              type,
-              content,
-              referenced_message,
-              timestamp,
-              sticker_items,
-              author,
-            } = message;
-
-            const { content: message_content } = referenced_message ?? {};
-            const { id: author_id } = author;
-
-            if (author.bot) continue;
-            if (sticker_items && sticker_items.length > 0) continue;
-
-            if (message.id === firstMessage?.id) {
-              const profile = await getProfile({
-                externalId: owner_id,
-                workspaceId,
-              });
-
-              if (!profile) continue;
-
-              await createActivity({
-                externalId: thread.id,
-                activityTypeKey: "discord:thread",
-                title: name,
-                message: type === 21 ? (message_content ?? "") : content,
-                memberId: profile.memberId,
-                channelId: channel.id,
-                createdAt: new Date(create_timestamp ?? ""),
-                updatedAt: new Date(create_timestamp ?? ""),
-                source: "Discord",
-                workspaceId,
-              });
-              break;
-            }
-
-            const profile = await getProfile({
-              externalId: author_id,
-              workspaceId,
-            });
-
-            if (!profile) continue;
-
-            await createActivity({
-              externalId: message.id,
-              activityTypeKey: "discord:reply_thread",
-              message: content,
-              replyTo: thread.id,
-              memberId: profile.memberId,
-              channelId: channel.id,
-              createdAt: new Date(timestamp),
-              updatedAt: new Date(timestamp),
-              source: "Discord",
-              workspaceId,
-            });
-          }
-
-          if (messages.length < 100) break;
-          beforeMessageId = messages.at(-1)?.id;
-        }
-      }
+      allThreads.push(...threads);
 
       if (threads.length < 100) break;
       before = threads.at(-1)?.thread_metadata?.archive_timestamp;
@@ -158,6 +73,99 @@ export const createManyArchivedThreads = async ({
 
         break;
       }
+    }
+  }
+
+  logger.info("All threads", {
+    count: allThreads.length,
+    allThreads,
+  });
+
+  for (const thread of allThreads) {
+    const { name, owner_id, thread_metadata } = thread;
+    const { create_timestamp } = thread_metadata ?? {};
+
+    if (!owner_id) continue;
+
+    let beforeMessageId: string | undefined = undefined;
+    let firstMessage: APIMessage | undefined;
+
+    while (true) {
+      const params = new URLSearchParams({
+        limit: "100",
+        ...(beforeMessageId ? { before: beforeMessageId } : {}),
+      });
+
+      const messages = (await discordClient.get(
+        `${Routes.channelMessages(thread.id)}?${params.toString()}`,
+      )) as APIMessage[];
+
+      logger.info("Archived thread messages", { messages });
+
+      if (messages.length < 100) firstMessage = messages.at(-1);
+
+      for (const message of messages) {
+        const {
+          type,
+          content,
+          referenced_message,
+          timestamp,
+          sticker_items,
+          author,
+        } = message;
+
+        const { content: message_content } = referenced_message ?? {};
+        const { id: author_id } = author;
+
+        if (author.bot) continue;
+        if (sticker_items && sticker_items.length > 0) continue;
+
+        if (message.id === firstMessage?.id) {
+          const profile = await getProfile({
+            externalId: owner_id,
+            workspaceId,
+          });
+
+          if (!profile) continue;
+
+          await createActivity({
+            externalId: thread.id,
+            activityTypeKey: "discord:thread",
+            title: name,
+            message: type === 21 ? (message_content ?? "") : content,
+            memberId: profile.memberId,
+            channelId: channel.id,
+            createdAt: new Date(create_timestamp ?? ""),
+            updatedAt: new Date(create_timestamp ?? ""),
+            source: "Discord",
+            workspaceId,
+          });
+          break;
+        }
+
+        const profile = await getProfile({
+          externalId: author_id,
+          workspaceId,
+        });
+
+        if (!profile) continue;
+
+        await createActivity({
+          externalId: message.id,
+          activityTypeKey: "discord:reply_thread",
+          message: content,
+          replyTo: thread.id,
+          memberId: profile.memberId,
+          channelId: channel.id,
+          createdAt: new Date(timestamp),
+          updatedAt: new Date(timestamp),
+          source: "Discord",
+          workspaceId,
+        });
+      }
+
+      if (messages.length < 100) break;
+      beforeMessageId = messages.at(-1)?.id;
     }
   }
 };
