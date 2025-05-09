a8f6fb7b1a128b8cfcff102f0b1dcc32ea8b38ba
ThomasBnf <thomas.bnfls@gmail.com>
2025-05-09 22:24:36 +0200

    sort profiles

--- apps/app/features/members/ProfilesParser.tsx
+++ apps/app/features/members/ProfilesParser.tsx
@@ -24,54 +24,58 @@ export const ProfilesParser = ({ profiles }: Props) => {
     <div className="space-y-4 p-4">
       <Label className="text-base">Profiles</Label>
       <div className="space-y-2">
-        {profiles?.map((profile) => {
-          const { source } = profile.attributes;
+        {profiles
+          ?.sort((a, b) =>
+            a.attributes.source.localeCompare(b.attributes.source),
+          )
+          .map((profile) => {
+            const { source } = profile.attributes;
 
-          switch (source) {
-            case "Discord":
-              return (
-                <DiscordSection
-                  key={profile.id}
-                  profile={profile as DiscordProfile}
-                />
-              );
-            case "Discourse":
-              return (
-                <DiscourseSection
-                  key={profile.id}
-                  profile={profile as DiscourseProfile}
-                />
-              );
-            case "Github":
-              return (
-                <GithubSection
-                  key={profile.id}
-                  profile={profile as GithubProfile}
-                />
-              );
-            case "Livestorm":
-              return (
-                <LivestormSection
-                  key={profile.id}
-                  profile={profile as LivestormProfile}
-                />
-              );
-            case "Slack":
-              return (
-                <SlackSection
-                  key={profile.id}
-                  profile={profile as SlackProfile}
-                />
-              );
-            case "Twitter":
-              return (
-                <TwitterSection
-                  key={profile.id}
-                  profile={profile as TwitterProfile}
-                />
-              );
-          }
-        })}
+            switch (source) {
+              case "Discord":
+                return (
+                  <DiscordSection
+                    key={profile.id}
+                    profile={profile as DiscordProfile}
+                  />
+                );
+              case "Discourse":
+                return (
+                  <DiscourseSection
+                    key={profile.id}
+                    profile={profile as DiscourseProfile}
+                  />
+                );
+              case "Github":
+                return (
+                  <GithubSection
+                    key={profile.id}
+                    profile={profile as GithubProfile}
+                  />
+                );
+              case "Livestorm":
+                return (
+                  <LivestormSection
+                    key={profile.id}
+                    profile={profile as LivestormProfile}
+                  />
+                );
+              case "Slack":
+                return (
+                  <SlackSection
+                    key={profile.id}
+                    profile={profile as SlackProfile}
+                  />
+                );
+              case "Twitter":
+                return (
+                  <TwitterSection
+                    key={profile.id}
+                    profile={profile as TwitterProfile}
+                  />
+                );
+            }
+          })}
       </div>
     </div>
   );
