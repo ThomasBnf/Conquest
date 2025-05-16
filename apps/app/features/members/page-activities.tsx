5c648e56a0578365ebe8eaa42205812e09f98be6
ThomasBnf <thomas.bnfls@gmail.com>
2025-05-16 12:31:03 +0200

    remove log

--- apps/app/features/members/page-activities.tsx
+++ apps/app/features/members/page-activities.tsx
@@ -21,13 +21,10 @@ export const PageActivities = ({ slug, memberId }: Props) => {
     isLoading: _isLoading,
     fetchNextPage,
     hasNextPage,
-    failureReason,
   } = trpc.activities.listInfinite.useInfiniteQuery(
     { memberId },
     { getNextPageParam: (_, allPages) => allPages.length * 25 },
   );
-
-  console.log(failureReason);
 
   const activities = data?.pages.flat();
 
