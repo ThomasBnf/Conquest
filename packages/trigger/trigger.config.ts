d76fbb8a0314bb5f64ed3441511951a2514a764c
ThomasBnf <thomas.bnfls@gmail.com>
2025-05-23 23:41:22 +0200

    (try)

--- packages/trigger/trigger.config.ts
+++ packages/trigger/trigger.config.ts
@@ -5,7 +5,12 @@ export default defineConfig({
   project: "proj_svkvhdhlspmnalydmzbs",
   logLevel: "log",
   build: {
-    external: ["react", "react-dom"],
+    external: [
+      "react",
+      "react-dom",
+      "@react-email/render",
+      "@react-email/components",
+    ],
     extensions: [
       prismaExtension({
         schema: "../../packages/db/prisma/schema.prisma",
