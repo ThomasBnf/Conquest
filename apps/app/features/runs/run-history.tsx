f408649dcbbb8a08fa7e12a653775afa794fc2fb
ThomasBnf <thomas.bnfls@gmail.com>
2025-06-21 11:48:03 +0200

    try

--- apps/app/features/runs/run-history.tsx
+++ apps/app/features/runs/run-history.tsx
@@ -11,14 +11,14 @@ export const RunHistory = ({ workflowId }: Props) => {
   const { data } = trpc.runs.list.useQuery({ workflowId });
 
   return (
-    <>
-      <div className="flex h-10 items-center">
+    <div className="flex flex-col h-full">
+      <div className="flex items-center h-10">
         <p className="p-2 font-medium">Runs history</p>
       </div>
       {data?.length === 0 ? (
         <EmptyRuns />
       ) : (
-        <ScrollArea className="h-full">
+        <ScrollArea className="flex-1">
           {data
             ?.slice()
             .reverse()
@@ -27,6 +27,6 @@ export const RunHistory = ({ workflowId }: Props) => {
             ))}
         </ScrollArea>
       )}
-    </>
+    </div>
   );
 };
--- apps/app/features/runs/run-sidebar.tsx
+++ apps/app/features/runs/run-sidebar.tsx
@@ -8,7 +8,7 @@ type Props = {
 export const RunSidebar = ({ workflowId, children }: Props) => {
   return (
     <div className="flex h-full w-full max-w-sm flex-col divide-y border-l bg-background">
-      {children}
+      <div className="h-full flex-1 overflow-hidden">{children}</div>
       <RunOverview workflowId={workflowId} />
     </div>
   );
